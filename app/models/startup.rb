5144ef570b0bfe495eaaf52b9e8c4edbc88ea803
BarbaraKuofie <68664224+BarbaraKuofie@users.noreply.github.com>
2020-09-19 17:13:40 -0500

    Add files via upload

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,49 @@
+require 'pry'
 class Startup
+attr_accessor :name
+attr_reader :founder, :domain, :fundinground, :venturecapitalist
+
+    @@all = []
+
+    def initialize(name, founder, domain, fundinground, venturecapitalist)
+        @name = name 
+        @founder = founder
+        @domain = domain 
+        @fundinground = fundinground
+        @venturecapitalist = venturecapitalist
+        @@all << self 
+    end 
+
+    def pivot(new_domain, new_name)
+        @domain = new_domain
+        @name = new_name 
+    end 
+
+    def self.all 
+        @@all 
+    end 
+
+    def self.find_by_founder(founder_name)
+        @@all.find{|element| element.founder == founder_name}
+    end 
+
+    def self.domains 
+       self.all.map{|startup| startup.domain}
+    end 
+
+    def sign_contract(venturecapitalist, type, investment)
+        fundinground = FundingRound.new(self, venturecapitalist, type, investment )
+    end 
+
+    ## looks into each instance of a founding round 
+    def num_funding_rounds 
+      FundingRound.all.select{|fundinground| fundinground.startup == self}.count
+    end 
+    
+    def self.total_funds
+        binding.pry 
+       total = fundinground.map {|fundinground| fundinground.investment += fundinground.investment}
+       total
+    end 
 
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,38 @@
 class VentureCapitalist
+attr_accessor :name, :total_worth
+@@all = []
 
+    def initialize (name, total_worth)
+        @name = name 
+        @total_worth = total_worth 
+        @@all << self 
+
+    end 
+
+    def self.all
+        @@all 
+    end
+
+    def self.tres_commas_club  
+         self.all.select{|vc_instance| vc_instance.total_worth > 1000000000}
+    end 
+
+    def portfolio 
+       all_rounds = FundingRound.all.select{|fundinground|fundinground.venture_capitalist == self}
+       unique_list = all_rounds.map{|fundinground|fundinground.startup}.uniq 
+       unique_list
+    end 
+
+    def biggest_investment 
+       all_rounds = FundingRound.all.select{|fundinground|fundinground.venture_capitalist == self}
+       largest_fund = all_rounds.map{|fundinground|fundinground.investment}.max 
+       largest_fund
+    end 
+
+    def invested(domain)
+        all_rounds = FundingRound.all.select{|fundinground|fundinground.venture_capitalist == self}
+        all_rounds = all_rounds.select{|fundinground| fundinground.startup.domain == domain}
+        total_invested = all_rounds.map{|fundinground| fundinground.investment}.sum 
+        total_invested
+    end 
 end
